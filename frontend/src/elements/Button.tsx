706ae04d6db663e0190005438b97917c2604584b
Johnny <ramirezjohnny@icloud.com>
2024-05-15 22:29:53 -0400

    Add more styles for Button

--- frontend/src/elements/Button.tsx
+++ frontend/src/elements/Button.tsx
@@ -3,8 +3,11 @@ import { styled } from "@mui/material/styles";
 
 const Button = styled(MuiButton)<ButtonProps>(({ theme }) => ({
   backgroundColor: theme.palette.primary.main,
-
+  textAlign: 'left',
   color: "white",
+  "&:hover": {
+    backgroundColor: theme.palette.primary.main,
+  },
 }));
 
 export default Button;
