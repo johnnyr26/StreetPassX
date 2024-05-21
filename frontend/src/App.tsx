f4c29cc22fa5296ee2675bb33bec782dc5290b6a
Johnny <ramirezjohnny@icloud.com>
2024-05-20 22:54:49 -0400

    Craete Lineup page

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,66 +1,7 @@
 import "./App.css";
 
-import Box from "@mui/material/Box";
+import Home from "./pages/Home";
 
-import Pass from "./components/Pass";
-import NavBar from "./components/Navbar";
-import Sidebar from "./components/Sidebar";
-
-const App = () => {
-  return (
-    <Box sx={{
-      height: '100%',
-      display: 'flex',
-      flexDirection: 'column',
-    }}>
-      <NavBar />
-      <Box
-        sx={{
-          flex: 1,
-          display: "flex",
-          flexDirection: "row",
-        }}
-      >
-        <Sidebar />
-        {/* Allows the cards to wrap without being stretched */}
-        <Box>
-          <Box
-            sx={{
-              paddingTop: "30px",
-              display: "flex",
-              justifyContent: "center",
-              flexFlow: "row wrap",
-            }}
-          >
-            <Pass
-              name="Jalen Jones"
-              passes={["1x Sunday Funday", "3x Saturday Night Out"]}
-            />
-            <Pass
-              name="Nate Drogin"
-              passes={["1x Sunday Funday", "3x Saturday Night Out"]}
-            />
-            <Pass
-              name="Walker Dubrueil"
-              passes={["1x Sunday Funday", "3x Saturday Night Out"]}
-            />
-            <Pass
-              name="Christopher Florance"
-              passes={["1x Sunday Funday", "3x Saturday Night Out"]}
-            />
-            <Pass
-              name="Hugh Markham"
-              passes={["1x Sunday Funday", "3x Saturday Night Out"]}
-            />
-            <Pass
-              name="Jack Feise"
-              passes={["1x Sunday Funday", "3x Saturday Night Out"]}
-            />
-          </Box>
-        </Box>
-      </Box>
-    </Box>
-  );
-};
+const App = Home;
 
 export default App;
--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -2,15 +2,28 @@ import './index.css'
 
 import React from 'react'
 import ReactDOM from 'react-dom/client'
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import App from './App.tsx'
+import Lineup from './pages/Lineup.tsx';
 
 import { theme } from '../style/theme.ts'
 import { ThemeProvider } from '@mui/material'
 
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App />
+  },
+  {
+    path: "/lineup",
+    element: <Lineup />
+  }
+]);
+
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
     <ThemeProvider theme={theme}>
-      <App />
+      <RouterProvider router={router} />
     </ThemeProvider>
   </React.StrictMode>
 );
