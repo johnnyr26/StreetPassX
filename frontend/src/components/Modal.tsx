09db68a4f29b27af77f7a41943b79c663563811a
Johnny <ramirezjohnny@icloud.com>
2024-05-23 18:35:00 -0400

    Create Floor page, make components for  specific passes, update modal

--- frontend/src/components/Modal.tsx
+++ frontend/src/components/Modal.tsx
@@ -1,15 +1,19 @@
 import { useState } from "react";
 import {
   Box,
+  Button as MuiButton,
   Checkbox,
   FormGroup,
   FormControlLabel,
   Modal as MuiModal,
   TextField,
+  Tooltip,
   Typography,
 } from "@mui/material";
 import { DatePicker } from "@mui/x-date-pickers/DatePicker";
 import Button from "../elements/Button";
+
+import HelpIcon from "@mui/icons-material/Help";
 
 const style = {
   position: "absolute",
@@ -19,6 +23,7 @@ const style = {
   bgcolor: "background.paper",
   borderRadius: "15px",
   boxShadow: 24,
+  width: "500px",
   p: 4,
 };
 
@@ -43,20 +48,26 @@ const Modal = ({
   return (
     <MuiModal open={open} onClose={handleClose}>
       <Box sx={style}>
-        <Typography variant="h6" component="h2">
-          Add New Event
+        <Typography
+          variant="h6"
+          component="h2"
+          sx={{
+            mb: "10px",
+          }}
+        >
+          New Trade Request
         </Typography>
-        <FormGroup>
-          <TextField
-            fullWidth
-            id="standard-basic"
-            label="Trading Away"
-            variant="standard"
-          />
+        <FormGroup
+          sx={{
+            outline: "none !important",
+          }}
+        >
+          <TextField fullWidth label="Trading Away" variant="standard" />
           <Box
             sx={{
               display: "flex",
               flexDirection: "row",
+              alignItems: "center",
               mt: "20px",
             }}
           >
@@ -66,13 +77,18 @@ const Modal = ({
                   onChange={() => setTradeAwayChecked((prev) => !prev)}
                 />
               }
+              sx={{
+                mr: "5px",
+              }}
               label="Specific Date?"
             />
+            <Tooltip title="Setting a date will help with notifying users when to add members to the list.">
+              <HelpIcon color="primary" sx={{ mr: "15px" }} />
+            </Tooltip>
             {tradeAwayChecked && <DatePicker />}
           </Box>
           <TextField
             fullWidth
-            id="standard-basic"
             label="Trading For"
             variant="standard"
             sx={{
@@ -83,6 +99,7 @@ const Modal = ({
             sx={{
               display: "flex",
               flexDirection: "row",
+              alignItems: "center",
               mt: "20px",
             }}
           >
@@ -92,11 +109,33 @@ const Modal = ({
                   onChange={() => setTradeForChecked((prev) => !prev)}
                 />
               }
+              sx={{
+                mr: "5px",
+              }}
               label="Specific Date?"
             />
+            <Tooltip title="Setting a date will help with notifying users when to add members to the list.">
+              <HelpIcon color="primary" sx={{ mr: "15px" }} />
+            </Tooltip>
             {tradeForChecked && <DatePicker />}
           </Box>
-          <Button sx={{ mt: "14px", fontSize: "18px" }}>Submit</Button>
+          <TextField
+            fullWidth
+            multiline
+            label="Additional Note"
+            variant="standard"
+            inputProps={{
+              style: {
+                maxHeight: "200px",
+              },
+            }}
+          />
+          <Button
+            sx={{ mt: "24px", fontSize: "18px" }}
+            onClick={() => handleClose()}
+          >
+            Submit
+          </Button>
         </FormGroup>
       </Box>
     </MuiModal>
