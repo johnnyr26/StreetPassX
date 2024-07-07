4686668ebfa008ed4f019d3e4d3841f40336275e
Johnny <ramirezjohnny@icloud.com>
2024-07-07 13:36:12 -0700

    Clean up backend

---
+++ backend/api/models/passes/functions.py
@@ -0,0 +1,16 @@
+from backend.api.models.passes import Pass
+from backend.db import get_database
+
+db = get_database()
+passes = db['passes']
+
+# Fetches all of the passes created without the _id field
+def get_passes():
+   return list(passes.find({}, {'_id': 0}))
+
+# creates a new pass
+def create_pass(new_pass: Pass):
+   try:
+      passes.insert_one(new_pass.to_json())
+   except Exception as ex:
+      print(f"An error occured while attempting to create a new pass: {ex}")
