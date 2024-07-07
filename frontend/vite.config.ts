16a750eb9491cec947565f9cb7004a4222a7f188
Johnny <ramirezjohnny@icloud.com>
2024-07-07 15:00:34 -0700

    configure proxy

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -4,4 +4,16 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    proxy: {
+      // Using the proxy instance
+      '/passes': {
+        target: 'http://127.0.0.1:5000',
+        changeOrigin: true,
+        configure: (proxy, options) => {
+          // proxy will be an instance of 'http-proxy'
+        },
+      },
+    },
+  },
 })
