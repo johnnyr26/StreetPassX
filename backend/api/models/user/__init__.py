98627332727e08763454dbf1d7a5f2cee4d8ddc2
Johnny <ramirezjohnny@icloud.com>
2024-12-26 16:05:02 -0500

    add phone number field

--- backend/api/models/user/__init__.py
+++ backend/api/models/user/__init__.py
@@ -4,6 +4,7 @@ from pydantic import BaseModel, Field
 class User(BaseModel):
     name: object = Field()
     email: object = Field()
+    phone_number: object = Field()
 
     def to_json(self):
         return jsonable_encoder(self, exclude_none=True)
